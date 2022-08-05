9d20edfb736d62e38f593410d92ac6e12cf36fc0
Tobias Knopp <tobias.knopp@tuhh.de>
2022-08-05 11:26:14 +0200

    add more examples

--- examples/neel_simple_1D.jl
+++ examples/neel_simple_1D.jl
@@ -5,19 +5,19 @@ using Plots
 DCore = 20e-9;         # particle diameter in nm
 alpha = 0.1;           # damping coefficient
 kAnis = 11000;         # anisotropy constant
-t_length = 1000;       # length of time vector
-t_max = 2/25000;       # maximum evaluation time in seconds
 N = 20;                # maximum spherical harmonics index to be considered
 n = [1;0;0];           # anisotropy axis
 reltol = 1e-4
 abstol = 1e-6
 
-t = range(0,stop=t_max,length=t_length);
+fx = 25000;
+tLength = 1000;       # length of time vector
+tMax = 4/fx;          # maximum evaluation time in seconds
 
+t = range(0,stop=tMax,length=tLength);
 
-f = 25000;
 # Magnetic field for simulation 
-B =  t -> (0.012*[sin(2*pi*f*t); 0*t; 0*t]);
+B =  t -> (0.012*[sin(2*pi*fx*t); 0*t; 0*t]);
 
 @time t, y = simulationMNP(B, t; n, DCore, kAnis, N, reltol, abstol)
 
---
+++ examples/neel_simple_2D.jl
@@ -0,0 +1,43 @@
+using MNPDynamics
+using Plots
+
+# Parameters
+DCore = 20e-9;         # particle diameter in nm
+alpha = 0.1;           # damping coefficient
+kAnis = 11000;         # anisotropy constant
+N = 20;                # maximum spherical harmonics index to be considered
+n = [1;0;0];           # anisotropy axis
+reltol = 1e-4
+abstol = 1e-6
+
+# Excitation frequencies
+fx = 2.5e6 / 102
+fy = 2.5e6 / 96
+
+samplingRate = 2.5e6
+tLength = lcm(96,102);             # length of time vector
+tMax = lcm(96,102) / samplingRate; # maximum evaluation time in seconds
+
+@info tMax 
+@info tLength
+
+t = range(0, stop=tMax, length=tLength);
+
+# Magnetic field for simulation 
+B =  t -> (0.012*[sin(2*pi*fx*t); sin(2*pi*fy*t); 0*t]);
+
+@time t, y = simulationMNP(B, t; n, DCore, kAnis, N, reltol, abstol)
+
+p1 = plot(t, y[:,1])
+plot!(p1, t, y[:,2])
+plot!(p1, t, y[:,3])
+
+dt = diff(t);
+dxdt = diff(y[:,1])./dt;
+dydt = diff(y[:,2])./dt;
+dzdt = diff(y[:,3])./dt;
+p2 = plot(t[1:end-1], dxdt)
+plot!(p2, t[1:end-1], dydt)
+plot!(p2, t[1:end-1], dzdt)
+
+plot(p1,p2)
