dbfff0a22524bd18cffd6a57299b73dfb6a22e82
Documenter.jl <documenter@juliadocs.github.io>
2022-08-06 18:53:13 +0000

    build based on acfc933

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MNPDynamics","page":"Home","title":"MNPDynamics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation goes here.","category":"page"}]
+}
