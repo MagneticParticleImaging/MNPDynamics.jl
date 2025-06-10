6a6222388aea6255a281bd5802484564012bad3a
Jonas Schumacher <github735@jonasschumacher.de>
2025-06-10 12:41:16 +0200

    Add Aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using MNPDynamics
-using Test, LinearAlgebra, StaticArrays
+using Test, LinearAlgebra, StaticArrays, Aqua
+
+@testset "Aqua" begin
+  Aqua.test_all(MPIFiles)
+end
 
 include("accuracy.jl")
 include("multiParams.jl")
